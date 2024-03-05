22b518088c9c0f161e02d5a4a517450ad3a84334
Eyal <eyalbenatan@gmail.com>
2024-03-05 10:32:53 +0200

    15.2

--- src/vm.c
+++ src/vm.c
@@ -7,18 +7,35 @@
 VM vm;
 
 void initVM(void) {
-
+    vm.stackTop = vm.stack;
 }
 
 void freeVM(void) {
 
 }
 
+void push(Value value) {
+    *vm.stackTop = value;
+    vm.stackTop++;
+}
+
+Value pop(void) {
+    vm.stackTop--;
+    return *vm.stackTop;
+}
+
 static InterpretResult run(void) {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
     for (;;) {
-#ifndef DEBUG_TRACE_EXECUTION
+#ifdef DEBUG_TRACE_EXECUTION
+        printf("          ");
+        for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+            printf("[ ");
+            printValue(*slot);
+            printf(" ]");
+        }
+        printf("\n");
         disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 #endif
         uint8_t instruction;
@@ -26,10 +43,11 @@ static InterpretResult run(void) {
         {
         case OP_CONSTANT:
             Value constant = READ_CONSTANT();
-            printValue(constant);
-            printf("\n");
+            push(constant);
             break;
         case OP_RETURN:
+            printValue(pop());
+            printf("\n");
             return INTERPRET_OK;
         default:
             break;
