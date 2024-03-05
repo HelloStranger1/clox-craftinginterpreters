353a76f205d2f0601457c27ba2cc4a577e3494e1
Eyal <eyalbenatan@gmail.com>
2024-03-05 11:25:52 +0200

    End of chapter 15

--- src/vm.c
+++ src/vm.c
@@ -27,6 +27,14 @@ Value pop(void) {
 static InterpretResult run(void) {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do { \
+        double b = pop(); \
+        double a = pop(); \
+        push(a op b); \
+    } while (false)
+
+
     for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
         printf("          ");
@@ -41,19 +49,41 @@ static InterpretResult run(void) {
         uint8_t instruction;
         switch (instruction = READ_BYTE())
         {
-        case OP_CONSTANT:
-            Value constant = READ_CONSTANT();
-            push(constant);
-            break;
-        case OP_RETURN:
-            printValue(pop());
-            printf("\n");
-            return INTERPRET_OK;
-        default:
-            break;
+            case OP_CONSTANT: {
+                Value constant = READ_CONSTANT();
+                push(constant);
+                break;
+            }
+            case OP_ADD: {
+                BINARY_OP(+);
+                break;    
+            }
+            case OP_SUBTRACT: {
+                BINARY_OP(-);
+                break;
+            }
+            case OP_MULTIPLY: {
+                BINARY_OP(*);
+                break;
+            }
+            case OP_DIVIDE: {
+                BINARY_OP(/);
+                break;
+            }
+            case OP_NEGATE: {
+                push(-pop()); break;
+            }
+            case OP_RETURN: {
+                printValue(pop());
+                printf("\n");
+                return INTERPRET_OK;
+            }
+            default:
+                break;
         }
     }
 #undef READ_BYTE
+#undef BINARY_OP
 #undef READ_CONSTANT
 }
 
